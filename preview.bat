a42491156880fb2e0bd9b205ab2da03de391d0bf
Yonggang Luo <luoyonggang@gmail.com>
2013-01-13 05:22:45 +0800

    Add preview script, using UTF8 as default.

--- preview.bat
+++ preview.bat
@@ -3,7 +3,7 @@ set LANG=zh_CN.UTF-8
 set LC_ALL=zh_CN.UTF-8
 call rake generate
 set PATH=%PATH%;C:\Program Files (x86)\Git\cmd\
-call rake deploy
+call rake preview
 if "%SYNC_STARTED%" EQU "SYNC_STARTED" goto finishd
 set SYNC_STARTED=SYNC_STARTED
 cmd /k
